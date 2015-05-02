73fd32760236d00f5178546ecf948f1c4e3092d9
Jack Burns <jack@raptiq.com>
2015-05-02 00:43:05 -0700

    finished responsive design and style tweaks

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', init, false);
+$(document).ready(init);
 
 function init() {
+
 	window.onscroll = function() {
 	    var viewportHieght = window.innerHeight || document.documentElement.clientHeight;
 
 	    // get ratio of scroll depth to screen size and use that to fade div
-	    var fade = 1 - ((getScrollTop() / viewportHieght) * 1.5);
+	    var fade = 1 - ((getScrollTop() / viewportHieght) * 2);
 
-		document.getElementById('banner').style.opacity = "" + fade;
+		document.getElementById('banner').style.opacity = '' + fade;
 	}
+
+	$('#learn-more').on('click', function(e) {
+	    $('html, body').stop().animate({
+	        'scrollTop': $('#main-content').offset().top
+	    }, 600, 'swing');
+	});
+	
+    console.log('ready');
 }
 
 // get scroll offset
